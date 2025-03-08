47c1cdbc56708a420c7448d439c9aa366ea3e8e5
RebornedTigrex <157234124+RebornedTigrex@users.noreply.github.com>
2025-03-08 12:44:33 +0300

    0.0.1

--- scr/core/utils/_configExceptions.py
+++ scr/core/utils/_configExceptions.py
@@ -1,8 +1,35 @@
+from typing import Optional, cast, Any
+
 
 class TigrexsError(Exception):
     pass
 
-class configMissing(TigrexsError):
+class configMiss(TigrexsError): # TODO:Если что-то окажется бесполезным - вырезать
+    message: str
+
+    body: object | None
+    """
+    Create new config, or fix exsits.
+    """
+
+    code: Optional[str] = None
+    param: Optional[str] = None
+    type: Optional[str]
     
-    
+    def __init__(self, message: str, *, body: object | None) -> None:
+        super().__init__(message)
+        self.message = message
+        self.body = body
+
+        # if is_dict(body):
+        #     self.code = cast(Any, construct_type(type_=Optional[str], value=body.get("code")))
+        #     self.param = cast(Any, construct_type(type_=Optional[str], value=body.get("param")))
+        #     self.type = cast(Any, construct_type(type_=str, value=body.get("type")))
+        # else:
+        #     self.code = None
+        #     self.param = None
+        #     self.type = None
+        
+class configParamMiss(TigrexsError):
+    message : str
     
--- scr/core/utils/configActions.py
+++ scr/core/utils/configActions.py
@@ -23,23 +23,22 @@ class configActions:
         
         
     
-    def loadCfg(sefl):
+    def loadCfg(self):
         try:
             with open(self.config, 'r') as f:
                 self.config_data = json.load(f)
-            os.close(self.config)
             
-            if "parce_type" not in self.config_data:
-                raise Exception("ParamDoestExist:1:", "Configuration parameter 'parce_type' is missing. 'parce_type' set to default value.")
-            if "arg" not in self.config_data:
-                raise Exception("ParamDoestExist:2:", "Configuration parameter 'arg' is missing. 'arg' set to default value.")
+            # if "parce_type" not in self.config_data:
+            #     raise Exception("ParamDoestExist:1:", "Configuration parameter 'parce_type' is missing. 'parce_type' set to default value.")
+            # if "arg" not in self.config_data:
+            #     raise Exception("ParamDoestExist:2:", "Configuration parameter 'arg' is missing. 'arg' set to default value.")
             
         except json.JSONDecodeError:
             print(f"Invalid JSON'{self.config}' or file does not exist. Script is using default configuration.")
             
         except Exception as err:
             print(f"Panic! In metood {self._load_config.__name__}: {err.args}")
-            if err.args[0] == "ParamDoestExist:1:":
-                self.config_data["parce_type"] = self.config_base["parce_type"]
-            if err.args[0] == "ParamDoestExist:2:":
-                self.config_data["arg"] = self.config_base["arg"]
+            # if err.args[0] == "ParamDoestExist:1:":
+            #     self.config_data["parce_type"] = self.config_base["parce_type"]
+            # if err.args[0] == "ParamDoestExist:2:":
+            #     self.config_data["arg"] = self.config_base["arg"]
